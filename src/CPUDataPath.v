5b52e88fd509f195fe81331ec7fdda6e12523117
Abhilash Tuse <abhilashtuse@gmail.com>
2017-11-14 14:46:07 -0800

    Fix contol unit and data path, assign numbers to FF
    Signed-off-by: Abhilash Tuse <abhilashtuse@gmail.com>

--- src/CPUDataPath.v
+++ src/CPUDataPath.v
@@ -2,56 +2,74 @@
 module CPUDataPath(clk, reset);
     input clk, reset;
 
-    wire [31:0] PcOut, PcNext, PcIn, DOut1, DOut2, IR, Reg1, Reg2, AOut, D5, D7, sOut32;
+    wire [31:0] PcOut, PcNext, PcIn, DOut1, DOut2, IR, Reg1, Reg2, AOut, D5, D7, sOut32, Q13;
     wire RF_WE, DM_WE;
-    reg [31:0] A, B, Din, AImm, DAddIn, Q;
+    reg [31:0] Din, Q2, Q3, Q4, Q7, Q8, Q12;
     reg [15:0] Imm;
-    reg [5:0] OPC, Q2;
-    reg [4:0] RS1, RS2, RD, Q5, Q7;
+    reg [5:0] OPC, Q1, Q6;
+    reg [4:0] RS1, RS2, RD, Q5, Q9, Q14;
     parameter delay = 1;
 
     ProgramCounter pc (.PcOut(PcOut), .PcNext(PcNext), .clk(clk), .PcIn(PcIn), .reset(reset));
     InstructionMemory im (.DOut(IR), .AddrIn(PcOut), .clk(clk), .reset(reset));
     SignExtend se(.sOut32(sOut32),.sIn16(Imm), .reset(reset));
     RegisterFile rf(.DOut1(Reg1),.DOut2(Reg2), .AddrIn1(RS1),
-    	          .AddrIn2(RS2),.AddrIn3(Q7),.Din(Din), .WE(WE), .clk(clk), .reset(reset));
-    ALU alu(.result(AOut), .a(A), .b(B), .Imm(AImm), .alu_control(Q2), .reset(reset));
-    DataMemory dm(.DOut(), .AddrIn(DAddIn), .Din(Q), .WE(WE), .reset(reset));
-    ControlUnit cu(.RF_WE(RF_WE), .DM_WE(DM_WE), .OPC(OPC),.reset(reset), .clk(clk));
+    	          .AddrIn2(RS2),.AddrIn3(Q14),.Din(Q13), .WE(WE), .clk(clk), .reset(reset));
+    ALU alu(.result(AOut), .a(Q2), .b(Q3), .Imm(Q4), .alu_control(Q1), .reset(reset));
+    DataMemory dm(.DOut(), .AddrIn(Q8), .Din(Q7), .WE(WE), .reset(reset));
+    ControlUnit cu(.RF_WE(RF_WE), .DM_WE(DM_WE), .Q13(Q13), .Q6(Q6), .Q8(Q8), .reset(reset), .clk(clk));
 
     always @(posedge clk or reset)
         begin
             if(reset == 1) begin
+            //RF stage
                 OPC <= 0;
                 RS1 <= 0;
                 RS2 <= 0;
                 RD <= 0;
-                Q2 <= 0;
-                A <= 0;
-                B <= 0;
                 Imm <= 0;
-                AImm <= 0;
-                Din <= 0;
+
+            //ALU stage
+                Q1 <= 0;
+                Q2 <= 0;
+                Q3 <= 0;
+                Q4 <= 0;
                 Q5 <= 0;
+
+            //DM stage
+                Q6 <= 0;
                 Q7 <= 0;
-                Q <= 0;
-                DAddIn <= 0;
+                Q8 <= 0;
+                Q9 <= 0;
+
+            //WB stage
+                Q14 <= 0;
+
             end
             if(reset == 0) begin
+            //RF stage
                 OPC <= IR [31:26];
                 RS1 <= IR [25:21];
                 RS2 <= IR [20:16];
                 RD <= IR [15:11];
                 Imm <= IR [15:0];
-                Q2 <= OPC;
-                A <= Reg1;
-                B <= Reg2;
-                AImm <= sOut32;
-                Din <= AOut;
+
+            //ALU stage
+                Q1 <= OPC;
+                Q2 <= Reg1;
+                Q3 <= Reg2;
+                Q4 <= sOut32;
                 Q5 <= RD;
-                Q7 <= Q5;
-                Q <= A;
-                DAddIn <= AOut;
+
+            //DM stage
+                Q6 <= Q1;
+                Q7 <= Q2;
+                Q8 <= AOut;
+                Q9 <= Q5;
+
+            //WB stage
+                Q14 <= Q9;
+                Q12 <= Q8;
             end
         end
 endmodule
--- src/ControlUnit.v
+++ src/ControlUnit.v
@@ -1,33 +1,39 @@
-module ControlUnit(RF_WE, DM_WE, OPC, reset, clk);
-input[5:0] OPC;
-input reset, clk;
-output RF_WE, DM_WE;
+module ControlUnit(RF_WE, DM_WE, Q13, Q6, Q8, reset, clk);
+    input[5:0] Q6;
+    input[31:0] Q8;
+    input reset, clk;
+    output RF_WE, DM_WE;
+    output [31:0] Q13;
 
-wire[5:0] D6, D10;
-reg[5:0] Q6, Q10;
-reg RF_WE, DM_WE;
+    wire[5:0] D10;
+    reg[5:0] Q10;
+    reg[31:0] Q13;
+    reg RF_WE, DM_WE;
 
-always @(posedge clk or reset)
-begin
-    if(reset == 1) begin
-        RF_WE <= 0;
-        DM_WE <= 0;
+    always @(posedge clk or posedge reset)
+    begin
+        if(reset == 1) begin
+            RF_WE <= 0;
+            DM_WE <= 0;
+        end
+        else begin
+            if(reset == 0) begin
+                case(Q6)
+                    1,2: begin
+                            Q10 <= Q6;
+                            Q13 <= Q8;
+                            //$display(Q10);
+                            case (Q6)
+                                1 : RF_WE <= 1; //ADD
+                                2 : RF_WE <= 1; //SUB
+                            endcase
+                        end
+                    3: begin
+                            DM_WE <= 1; //Store
+                        end
+                endcase
+            end
+        end
     end
-    else begin
-        Q6 <= OPC;
-        case(Q6)
-            1: begin
-                    Q10 <= Q6;
-                    case (Q10)
-                        1 : RF_WE <= 1; //ADD
-                        2 : RF_WE <= 1; //SUB
-                    endcase
-                end
-            3: begin
-                    DM_WE <= 1; //Store
-                end
-        endcase
-    end
-end
 
 endmodule
